3f262bc98cea0c983c210495599019e145bb1435
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2023-12-16 18:11:45 +0700

    vercel deploy part 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/",
+    base: "",
     build: {
         target: "es2020",
     },
+    assetsInclude: ["**/*.png", "**/*.jpg", "**/*.svg", "**/*.gif", "**/*.ico", "**/*.json", "**/*.bin"],
 });
